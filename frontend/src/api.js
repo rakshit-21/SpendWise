bb773b7391b451fc2a8ae27e4e23bee7781f5f64
rakshit suneja <rakshits2105@gmail.com>
2026-05-29 16:33:33 +0530

    point frontend to render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://spendwise-90v0.onrender.com/api',  // ← your Render URL
+  baseURL: 'https://spendwise-90v0.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
